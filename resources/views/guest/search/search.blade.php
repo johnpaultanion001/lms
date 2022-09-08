cd21d3c892cce8d7138ef4ae34631a8189b7812a
Alyssie Öö <caluyaalyssamae@gmail.com>
2022-09-08 23:09:22 +0800

    Report a problem

--- resources/views/guest/search/search.blade.php
+++ resources/views/guest/search/search.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.guest')
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('search') }}"><img src="{{ asset('public/assets/img/ekyc-logo.png') }}" alt="Logo" class="the-logo"> EKYC</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ url('search') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('documentation') }}">Documentation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 @section('content')
 <style>
     body{
